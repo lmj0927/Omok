955c70f4f36aca2f96c2ad6481ce1f51e52f0f3a
ToastBox <car9245@naver.com>
2025-03-13 14:30:15 +0900

    [Chore] 플레이어 정보와 리더보드 랭킹 분리표기

--- Assets/Scripts/UI/LeaderBoardPanelController.cs
+++ Assets/Scripts/UI/LeaderBoardPanelController.cs
@@ -5,52 +5,57 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LeaderBoardPanelController : MonoBehaviour
+public class LeaderBoardPanelController : PanelController
 {
+    [SerializeField] private Image profileImage;
+    [SerializeField] private Sprite[] profileSprites;
+    [SerializeField] private TMP_Text userInfoText;
+    [SerializeField] private TMP_Text userWinRateText;
+    
     [SerializeField] private GameObject rankCellPrefab;
     [SerializeField] private RectTransform contentTransform;
-    [SerializeField] private float cellSize; //
+    [SerializeField] private float cellSize; //140
     
     [SerializeField] private Button closeButton;
-    UserInfo userInfo;
+    UserInfo _userInfo;
     
     private void Start()
     {
-        closeButton.onClick.AddListener(()=>Hide());
+        closeButton.onClick.AddListener(OnClickCloseButton);
+        Show();
+        InitUserCell();
+        CreateRankCell();
+    }
+
+    void InitUserCell()
+    {
+        _userInfo = GameManager.Instance.userInfo;
+        
+        //TODO: 랭크 계산 후 순위 적용
+
+        int index = _userInfo.profileIndex;
+        profileImage.sprite = profileSprites[index];
+        
+        float winRate = (float)_userInfo.winCount / (_userInfo.winCount + _userInfo.loseCount) * 100f;
+        userInfoText.text = $"{_userInfo.tier}급 {_userInfo.nickname}";
+        userWinRateText.text = $"{_userInfo.winCount}승 {_userInfo.loseCount}패 ({winRate:F0}%)";
     }
 
     void CreateRankCell()
     {
-        // StartCoroutine(NetworkManager.Instance.GetScore((userinfo) =>
-        // {
-        //     user = userinfo;
-        //     StartCoroutine(NetworkManager.Instance.GetLeaderboard((scores) =>
-        //     {
-        //         var rank = 1;
-        //         foreach (var scoreinfo in scores.scores)
-        //         {
-        //             var cell = Instantiate(rankCellPrefab, contentTransform); 
-        //             cell.GetComponent<RankInfoCell>().SetRankInfo(scoreinfo);
-        //             contentTransform.sizeDelta = new Vector2(0, cellSize*rank);
-        //             rank++;
-        //         }
-        //     }, () =>
-        //     {
-        //         Debug.Log("Failed to create rank cell");
-        //     }));
-        // }, () =>
-        // {
-        //
-        // }));
-    }
-    
-    public void Show()
-    {
-        gameObject.SetActive(true);
+        //임시코드
+        _userInfo = GameManager.Instance.userInfo;
+        
+        for (int i = 0; i < 20; i++)
+        {
+            var cell = Instantiate(rankCellPrefab, contentTransform); 
+            cell.GetComponent<RankInfoCell>().SetRankInfo(_userInfo);
+            contentTransform.sizeDelta = new Vector2(0, cellSize*i);
+        }
     }
 
-    public void Hide()
+    void OnClickCloseButton()
     {
-        gameObject.SetActive(false);
-    }   
+        Hide();
+    }
 }

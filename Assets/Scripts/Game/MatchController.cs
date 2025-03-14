7ed62bcf162846b0d3c5a6ceb1e254bcbab91fad
moon <ediamoon@naver.com>
2025-03-15 00:03:28 +0900

    [Fix] AI 차례인 경우에도 백색 예상 돌을 넣을 수 있는 경우 수정

--- Assets/Scripts/Game/MatchController.cs
+++ Assets/Scripts/Game/MatchController.cs
@@ -45,7 +45,7 @@ public class MatchController : IDisposable
     
     public void SetCurrentCell(Cell cell)
     {
-        if(!IsMyTurn() && _matchPlayType == PLAY_TYPE.Multi) return;
+        if(!IsMyTurn() && (_matchPlayType == PLAY_TYPE.Multi || _matchPlayType == PLAY_TYPE.AI)) return;
 
         if(currentCell != null)
             OnDrawCell?.Invoke(new TurnData{ row = currentCell.row, col = currentCell.col }, CELL_TYPE.None);

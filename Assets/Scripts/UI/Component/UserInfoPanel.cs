6c35b3dbbfe90e6327cacb06bd6bf15f99c11087
ToastBox <car9245@naver.com>
2025-03-17 16:42:15 +0900

    [Chore] 유저 프로필 기능 통합 및 일부수정

--- Assets/Scripts/UI/Component/UserInfoPanel.cs
+++ Assets/Scripts/UI/Component/UserInfoPanel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,11 +7,26 @@ using UnityEngine.UI;
 public class UserInfoPanel : MonoBehaviour
 {
     [SerializeField] TMP_Text infoText;
-    [SerializeField] ProfilePanel profilePanel;
+    [SerializeField] Image profileImage;
+    [SerializeField] List<Sprite> profileSprites;
     private UserInfo _userInfo;
 
     private void Awake()
     {
+        SetUserInfo(GameManager.Instance.GetUserInfo());
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.playerDataController.OnChangedProfile += SetProfileImage;
+    }
+
+    //설정창에서 갱신이 안되어서 활성화 될때 갱신시킴.
+    public void OnEnable()
+    {
+        //계정 변경 시 playerDataController가 초기화 될 경우 재등록
+        GameManager.Instance.playerDataController.OnChangedProfile ??= SetProfileImage;
+
         SetUserInfo(GameManager.Instance.GetUserInfo());
     }
 
@@ -21,6 +37,18 @@ public class UserInfoPanel : MonoBehaviour
         string infoStr = $"{_userInfo.tier}급 {_userInfo.nickname}";
         infoText.text = infoStr;
         
-        profilePanel.SetProfileImage(_userInfo.profileIndex);
+        SetProfileImage(_userInfo.profileIndex);
+    }
+    
+    public void SetProfileImage(int idx)
+    {
+        if (idx < 0 || idx >= profileSprites.Count)
+        {
+            Debug.Log("PRCNT"+profileSprites.Count);
+            Debug.LogError("Profile Index out of range." + idx);
+            return;
+        }
+        
+        profileImage.sprite = profileSprites[idx];
     }
 }

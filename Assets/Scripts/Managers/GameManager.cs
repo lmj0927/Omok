f71ae9657eaf4c4a36063ee9bda9be85b5461ec4
moon <ediamoon@naver.com>
2025-03-20 00:13:44 +0900

    [Fix] 프로그램 종료 시 OnDestroy에서 Spawner정리하도록 수정

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -72,15 +72,8 @@ public class GameManager : Singleton<GameManager>
 
     public new void OnDestroy()
     {
-        if(matchController != null)
-        {
-            //본인이 도망갔을 경우 처리 방향이지만, 처리 불가
-            // if(matchController.GetMatchState() != MATCH_STATE.End)
-            // {
-            //     matchController.Surrender();
-            // }
-            matchController.Dispose();
-        }
+        StoneSpawner?.StopSpawn();
+        matchController?.Dispose();
         base.OnDestroy();
     }
 }
